300718761a249b0943caeb6c5e34efddf9fe7e1f
RuslanAgishev <agishev_ruslan@mail.ru>
2019-10-28 13:06:30 +0300

    yolo object detection based on pyimagesearch tutorial

---
+++ Objects_Detection/YOLO/image_inference.sh
@@ -0,0 +1,2 @@
+
+python yolo.py --image images/example_01.png --yolo yolo-coco
---
+++ Objects_Detection/YOLO/video_inference.sh
@@ -0,0 +1,2 @@
+ 
+python yolo_video.py --input videos/car_chase_01.mp4 --output output/car_chase_01_output.avi --yolo yolo-coco
