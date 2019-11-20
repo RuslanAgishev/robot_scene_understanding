ea2c03e09de67eae4d1bf21c2b08dbad60f1c9ac
RuslanAgishev <agishev_ruslan@mail.ru>
2019-11-20 17:54:18 +0300

    object detection pipeline based on torchvision tutorial

---
+++ Objects_Detection/Torchvision/getHelperFunction.sh
@@ -0,0 +1,11 @@
+git clone https://github.com/pytorch/vision.git
+cd vision
+git checkout v0.3.0
+
+cp references/detection/utils.py ../
+cp references/detection/transforms.py ../
+cp references/detection/coco_eval.py ../
+cp references/detection/engine.py ../
+cp references/detection/coco_utils.py ../
+
+rm -rf vision
---
+++ Objects_Detection/Torchvision/installPycocotools.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/cocodataset/cocoapi.git
+cd cocoapi/PythonAPI
+python3 setup.py build_ext install
+rm -rf cocoapi
